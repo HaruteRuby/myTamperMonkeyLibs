e64a1affabea878472e9019c3916c47c58c4b379
HaruteRuby <41945025+HaruteRuby@users.noreply.github.com>
2021-09-18 17:34:41 +0800

    更新必修
    视频页

--- bixiu_video.user.js
+++ bixiu_video.user.js
@@ -9,33 +9,33 @@
 // ==/UserScript==
 
 (function() {
-  "use strict";
-  $("body").off("blur");
-  window.onblur = function() {};
-  $.fn.pointsVerify = function(options, callbacks) {
-    options.success();
-  };
-
-  function closeCurrentPage() {
-    var userAgent = navigator.userAgent;
-    if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
-      window.location.href = "about:blank";
-      window.close();
-    } else {
-      window.opener = null;
-      window.open("", "_self");
-      window.close();
+    "use strict";
+    $("body").off("blur");
+    window.onblur = function() {};
+    $.fn.pointsVerify = function(options, callbacks) {
+        options.success();
+    };
+    function closeCurrentPage() {
+        var userAgent = navigator.userAgent;
+        if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
+            window.location.href = "about:blank";
+            window.close();
+        } else {
+            window.opener = null;
+            window.open("", "_self");
+            window.close();
+        }
     }
-  }
-  function closePage() {
-    player.videoMute();
-    let data = player.getMetaDate();
-    let toal = data.duration + 120;
-    console.log("init");
-    setTimeout(closeCurrentPage, toal * 1000);
-  }
-
-  setTimeout(() => {
-    closePage();
-  }, 4 * 1000);
+    function closePage() {
+        player.videoMute();
+        player.videoSeek(60*120+20)
+        let data = player.getMetaDate();
+        let toal = data.duration + 120;
+        console.log("init");
+        //可以快速刷，不用等播完的，主要是等一下视频加载的时间，实在加载不出来干脆重新开这个页面可能还快一些。-2021年9月18日 17:23:12@HaruteRuby
+        setTimeout(closeCurrentPage, 60*1000);
+    }
+    setTimeout(() => {
+        closePage();
+    }, 4 * 1000);
 })();
